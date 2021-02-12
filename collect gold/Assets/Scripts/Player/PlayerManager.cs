3bcdd72f1b91fe1783c371089181e820b8ed68a9
cerkezbilal <bilal.karademir@hotmail.com>
2021-02-12 20:18:51 +0300

    initial commit

---
+++ collect gold/Assets/Scripts/Player/PlayerManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerManager : MonoBehaviour
+{
+    public static bool gameOver;
+    public GameObject gameOverPanel;
+    public static bool isGameStarted;
+    public static int numberOfCoins;
+    public Text coinsText;
+
+    public Text startingText;
+
+    void Start()
+    {
+        gameOver = false;
+        Time.timeScale = 1;
+        isGameStarted = false;
+        numberOfCoins = 0;
+        coinsText.text = "Score: "+numberOfCoins.ToString();
+    }
+
+    
+    void Update()
+    {
+        coinsText.text = "Score: " + numberOfCoins.ToString();
+
+        if (gameOver)
+        {
+            Time.timeScale = 0;
+            gameOverPanel.SetActive(true);
+        }
+        if (SwipeManager.tap)
+        {
+            isGameStarted = true;
+            Destroy(startingText);
+        }
+    }
+}
